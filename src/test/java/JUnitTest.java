b38f8046a27c8bfa23bd5fac6d51953695b6e9d8
Vakavava <93044466+Vakavava@users.noreply.github.com>
2022-06-10 04:02:02 +0300

    Add steps

--- src/test/java/JUnitTest.java
+++ src/test/java/JUnitTest.java
@@ -1,4 +1,6 @@
 import com.codeborne.selenide.Condition;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,37 +14,56 @@ import java.util.stream.Stream;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
+@Owner("vakavava")
 public class JUnitTest {
     @BeforeEach
     public void setup(){
         open("https://github.com/junit-team/junit4");
     }
 
+    @Feature("Переключение веток")
     @Test
     @DisplayName("Переход на ветку fixtures")
     public void shouldOpenFixturesBranch(){
-        TestPages.profilePage.branchDropdown()
-                .click();
-        TestPages.junitPage.repoFixturesButton()
-                .click();
-        TestPages.junitPage.commitsInBranchNotification()
-                .shouldBe(Condition.visible);
+        step("Нажать на дропдаун для перехода между веток", () -> {
+            TestPages.profilePage.branchDropdown()
+                    .click();
+        });
+        step("Выбрать ветку fixtures", () -> {
+            TestPages.junitPage.repoFixturesButton()
+                    .click();
+        });
+        step("Проверить, что открылась страница ветки fixtures", () -> {
+            TestPages.junitPage.commitsInBranchNotification()
+                    .shouldBe(Condition.visible);
+        });
     }
 
+    @Feature("Поиск релизов")
     @MethodSource("correctData")
     @ParameterizedTest(name = "по {0}")
     @DisplayName("Поиск релизов")
     public void shouldSearchReleasesByNameAndNumber(String type, String searchData, String searchResults) {
-        TestPages.junitPage.releasesButton()
-                .click();
-        TestPages.junitPage.tabReleases()
-                        .shouldBe(Condition.visible);
-        TestPages.junitPage.searchInput()
-                .sendKeys(searchData + Keys.ENTER);
-        TestPages.junitPage.nameOfRelease()
-                .shouldHave(text(searchResults));
+        step("Нажать на кнопку Releases", () -> {
+            TestPages.junitPage.releasesButton()
+                    .click();
+        });
+        step("Проверить, что открылась страница с релизами", () -> {
+            TestPages.junitPage.tabReleases()
+                    .shouldBe(Condition.visible);
+        });
+        step("Ввести в поле ввода данные и нажать enter", () -> {
+            TestPages.junitPage.searchInput()
+                    .sendKeys(searchData + Keys.ENTER);
+        });
+        step("Проверить, что на странице отобразились нужные релизы", () -> {
+            TestPages.junitPage.nameOfRelease()
+                    .shouldHave(text(searchResults));
+        });
+
     }
 
     static Stream<Arguments> correctData() {
--- src/test/java/ProfileTest.java
+++ src/test/java/ProfileTest.java
@@ -1,30 +1,53 @@
 import com.codeborne.selenide.Condition;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Story;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 
 public class ProfileTest {
+    @Owner("vakavava")
+    @Feature("Авторизация")
+    @Story("Успешная авторизация")
     @Test
     @DisplayName("Успешная авторизация")
     public void shouldOpenProfileTest() {
-        open("https://github.com/");
-        TestPages.authPage.mainSignInButton()
-                .click();
-        TestPages.authPage.logInInput()
-                .sendKeys("Vakavava");
-        TestPages.authPage.passwordInput()
-                .sendKeys("JingleBells1984");
-        TestPages.authPage.signInButton()
-                .click();
-        TestPages.profilePage.header()
-                .shouldBe(Condition.visible);
-        TestPages.profilePage.profileDropDown()
-                .click();
-        TestPages.profilePage.myProfileButton()
-                .click();
-        TestPages.profilePage.accountName()
-                .shouldHave(text("Vakavava"));
+        step("Открыть главную страницу гитхаб", () -> {
+            open("https://github.com/");
+        });
+        step("Нажать на кнопку Sign In", () -> {
+            TestPages.authPage.mainSignInButton()
+                    .click();
+        });
+        step("Ввести в поля ввода логина и пароля данные", () -> {
+            TestPages.authPage.logInInput()
+                    .sendKeys("Vakavava");
+            TestPages.authPage.passwordInput()
+                    .sendKeys("JingleBells1984");
+        });
+        step("Нажать на кнопку входа", () -> {
+            TestPages.authPage.signInButton()
+                    .click();
+        });
+        step("Проверить, что прошла авторизация", () -> {
+            TestPages.profilePage.header()
+                    .shouldBe(Condition.visible);
+        });
+        step("Нажать на дропдаун аккаунта в хэдере", () -> {
+            TestPages.profilePage.profileDropDown()
+                    .click();
+        });
+        step("Нажать на кнопку My Profile", () -> {
+            TestPages.profilePage.myProfileButton()
+                    .click();
+        });
+        step("Проверить, что на странице появилось имя аккаунта", () -> {
+            TestPages.profilePage.accountName()
+                    .shouldHave(text("Vakavava"));
+        });
     }
 }
